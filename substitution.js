ebe6ff1f786c47295b8645c1547ae9427bcec392
philippica <429844547@qq.com>
2017-08-18 01:14:31 +0800

    Update substitution.js

--- substitution.js
+++ substitution.js
@@ -1,5 +1,25 @@
+function pattern(str) {
+	var curLetter = 0;
+	var len = str.length;
+	var ret = "";
+	var usedLetter = new Array(26);
+	for(var i = 0; i < len; i++) {
+		console.info(usedLetter[str[i]]);
+		if(usedLetter[str[i]] == undefined) {
+			usedLetter[str[i]] = String.fromCharCode(curLetter + 97);
+			curLetter++;
+		}
+		ret += usedLetter[str[i]];
+	}
+	return ret;
+}
+
+
 function substituteSolver(text) {
 	tokens = text.split(/[^a-zA-Z0-9\']+/);
 	countOfTokens = tokens.length;
+	for(var i = 0; i < countOfTokens; i++) {
+		console.info(tokens[i]);
+	}
 	return tokens;
 }
