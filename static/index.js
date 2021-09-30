fcb1769cbd262725ea5585773cd97fd21fd6c467
Yuanjie Pu <yuapu@microsoft.com>
2021-09-30 16:25:45 +0800

    Signed-off-by: Yuanjie Pu <yuapu@microsoft.com>

---
+++ static/index.js
@@ -0,0 +1,1 @@
+import{SubstitutionSolver}from"./substitution/substitution";import{Nonogram}from"./nonogram/nonogram";import{OneWord}from"./one-word/one-word";import{WordSearch}from"./word-search/wordSearch";import{RubikCubeStage}from"./rubic-cube/index";export{SubstitutionSolver,OneWord,Nonogram,WordSearch,RubikCubeStage};
